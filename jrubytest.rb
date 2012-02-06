22b0b759312aa6254c1568c4380ee86728fe6278
Andreas Ronge <andreas.ronge@gmail.com>
2012-02-06 20:46:14 +0100

    Added missing dependency to Joda JAR - but test it still fails

--- jrubytest.rb
+++ jrubytest.rb
@@ -1,6 +1,11 @@
 require 'java'
 
 
+$CLASSPATH << File.expand_path(ENV['HOME'] + "/.m2/repository/joda-time/joda-time/2.0/joda-time-2.0.jar")
 $CLASSPATH << File.expand_path(Dir.pwd + '/target/classes')
+
+time = Java::OrgJodaTime::DateTime.new
+puts "Calling DateTime success"
+# THIS FAILS:
 # THIS DOES NOT WORK !
 Java::Bar::FromJava.getTime
